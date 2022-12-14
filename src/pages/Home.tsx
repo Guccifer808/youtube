03c658c85dbfc3e819d96a54afaff28328f49cf2
G808 <hucciallstars@gmail.com>
2022-12-15 01:29:21 +0200

    Added Prettier. Sidebar edits

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Sidebar from '../components/Sidebar';
 
 export default function Home() {
   return (
-    <div><Navbar /></div>
-  )
+    <div className="max-h-screen overflow-hidden">
+      <div style={{ height: '7.5vh' }}>
+        <Navbar />
+      </div>
+      <div className="flex" style={{ height: '92.5vh' }}>
+        <Sidebar />
+      </div>
+    </div>
+  );
 }
